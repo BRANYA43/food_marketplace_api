f566cfbdb5f60924b5ca8c20abd149f5317dab4e
BRANYA <tbd2504@gmail.com>
2024-06-02 18:21:27 +0300

    User custom APITestCase in RegisterViewTest

--- src/accounts/tests/test_view_sets/test_user_view_set.py
+++ src/accounts/tests/test_view_sets/test_user_view_set.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APITestCase
+from accounts.tests import APITestCase
 
 from accounts import services
 
@@ -12,46 +12,38 @@ class RegisterViewTest(APITestCase):
     def setUp(self) -> None:
         self.url = reverse('user-register')
         self.data = {
-            'email': 'rick.sanchez@test.com',
-            'password': 'qwe123!@#',
+            'email': self.TEST_EMAIL,
+            'password': self.TEST_PASSWORD,
             'phone': '+380123456789',
         }
 
-    def assert_not_allowed_methods(self, methods: list):
-        for method in methods:
-            response = getattr(self.client, method)(self.url)
-            self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
-
-    def login_user_by_token(self, user):
-        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + str(user.access_token))
-
     def test_view_allows_only_post_method(self):
         response = self.client.post(self.url, self.data)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assert_response_status(response, status.HTTP_201_CREATED)
 
-        self.assert_not_allowed_methods(['get', 'put', 'patch', 'delete'])
+        self.assert_not_allowed_methods(['get', 'put', 'patch', 'delete'], self.url)
 
     def test_view_is_accessed_for_unauthenticated_user(self):
         response = self.client.post(self.url, self.data)
 
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assert_response_status(response, status.HTTP_201_CREATED)
 
     def test_view_isnt_accessed_for_authenticated_user(self):
         del self.data['phone']
-        user = User.objects.create_user(**self.data)
+        user = self.create_test_user(**self.data)
         self.login_user_by_token(user)
 
         response = self.client.post(self.url, self.data)
 
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-        self.assertIn('User is already authenticated.', response.data.values())
+        self.assert_response_status(response, status.HTTP_403_FORBIDDEN)
+        self.assert_response_permission_error(response, 'User is already authenticated.')
 
     def test_view_register_user_with_valid_credentials(self):
         self.assertEqual(User.objects.count(), 0)
 
         response = self.client.post(self.url, self.data)
 
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assert_response_status(response, status.HTTP_201_CREATED)
         self.assertEqual(User.objects.count(), 1)
 
         user = User.objects.first()
@@ -71,7 +63,7 @@ class RegisterViewTest(APITestCase):
 
         response = self.client.post(self.url, invalid_data)
 
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assert_response_status(response, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(User.objects.count(), 0)
 
         errors = response.data.get('errors')
@@ -89,5 +81,5 @@ class RegisterViewTest(APITestCase):
 
         response = self.client.post(self.url, self.data)
 
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assert_response_status(response, status.HTTP_400_BAD_REQUEST)
         self.assertIn('errors', response.data)
