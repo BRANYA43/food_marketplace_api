45462896a66bb8d2082ab5071fff09f390ec94e1
BRANYA <tbd2504@gmail.com>
2024-06-03 15:42:36 +0300

    Add schema for UserViewSet

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.utils.module_loading import import_string
+from drf_spectacular.utils import extend_schema, OpenApiResponse
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
@@ -12,24 +13,32 @@ from accounts.permissions import IsUnauthenticated
 
 
 class UserViewSet(viewsets.ViewSet):
+    serializer_classes = {
+        'register': serializers.UserRegisterSerializer,
+        'login': import_string(settings.SIMPLE_JWT['TOKEN_OBTAIN_SERIALIZER']),
+        'logout': import_string(settings.SIMPLE_JWT['TOKEN_BLACKLIST_SERIALIZER']),
+        'refresh': import_string(settings.SIMPLE_JWT['TOKEN_REFRESH_SERIALIZER']),
+    }
+
     def get_serializer_class(self):
         """
         Return a serializer class by the action.
         """
-        match self.action:
-            case 'register':
-                return serializers.UserRegisterSerializer
-            case 'login':
-                return import_string(settings.SIMPLE_JWT['TOKEN_OBTAIN_SERIALIZER'])
-            case 'logout':
-                return import_string(settings.SIMPLE_JWT['TOKEN_BLACKLIST_SERIALIZER'])
-            case 'refresh':
-                return import_string(settings.SIMPLE_JWT['TOKEN_REFRESH_SERIALIZER'])
+        return self.serializer_classes[self.action]
 
     def get_serializer(self, *args, **kwargs):
         serializer = self.get_serializer_class()
         return serializer(*args, **kwargs)
 
+    @extend_schema(
+        operation_id='user_register',
+        description='Register user by email and password.',
+        responses={
+            201: OpenApiResponse(
+                description='User was registered successfully.',
+            ),
+        },
+    )
     @action(detail=False, methods=['post'], permission_classes=[IsUnauthenticated])
     def register(self, request):
         serializer = self.get_serializer(data=request.data)
@@ -39,14 +48,37 @@ class UserViewSet(viewsets.ViewSet):
         else:
             return Response(data={'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
+    @extend_schema(
+        operation_id='user_login',
+        description='Log a user in by creating new refresh and access user tokens.',
+        responses={
+            200: OpenApiResponse(description='User logged in successfully.', response=serializer_classes['login']),
+        },
+    )
     @action(detail=False, methods=['post'], permission_classes=[IsUnauthenticated])
     def login(self, request):
         return jwt_views.token_obtain_pair(request._request)
 
+    @extend_schema(
+        operation_id='user_logout',
+        description='Log a user out by adding all user tokens to the blacklist.',
+        responses={
+            200: OpenApiResponse(description='User logged out successfully.'),
+        },
+    )
     @action(detail=False, methods=['post'], permission_classes=[IsAuthenticated])
     def logout(self, request):
         return jwt_views.token_blacklist(request._request)
 
+    @extend_schema(
+        operation_id='user_refresh_token',
+        description='Refresh a expiry of user refresh token and return new user access token.',
+        responses={
+            200: OpenApiResponse(
+                description='Token was refreshed successfully.', response=serializer_classes['refresh']
+            ),
+        },
+    )
     @action(detail=False, methods=['post'], permission_classes=[AllowAny])
     def refresh(self, request):
         return jwt_views.token_refresh(request._request)
