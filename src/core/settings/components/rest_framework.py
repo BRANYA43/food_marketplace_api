0f82dc8acbe9ce38c0feb5c174ee3571be022c9d
BRANYA <tbd2504@gmail.com>
2024-06-03 13:32:34 +0300

    Add drf_spectacular settings

--- src/core/settings/components/rest_framework.py
+++ src/core/settings/components/rest_framework.py
@@ -7,14 +7,11 @@ from core.settings.components.base import INSTALLED_APPS
 
 INSTALLED_APPS += [
     'rest_framework',
-    'drf_spectacular',
 ]
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 10,
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',),
 }
