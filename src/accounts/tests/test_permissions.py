f4d80fa5de02221ef6c4230def6a44e8b26faa50
BRANYA <tbd2504@gmail.com>
2024-06-05 19:34:36 +0300

    Change getting request for premission test of unauthenticated user

--- src/accounts/tests/test_permissions.py
+++ src/accounts/tests/test_permissions.py
@@ -1,8 +1,6 @@
-from unittest.mock import MagicMock
-
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AnonymousUser
-from rest_framework.test import APITestCase
+from utils.tests import APITestCase
 
 from accounts import permissions
 
@@ -13,17 +11,13 @@ User = get_user_model()
 class IsUnauthenticatedPermissionTest(APITestCase):
     def setUp(self) -> None:
         self.permission = permissions.IsUnauthenticated()
-
-    def get_mock_request(self, user=AnonymousUser()):
-        request = MagicMock()
-        request.user = user
-        return request
+        self.request = self.request_factory.get('/')
+        self.request.user = AnonymousUser()
 
     def test_permission_passes_unauthenticated_user(self):
-        request = self.get_mock_request()
-        self.assertTrue(self.permission.has_permission(request, None))
+        self.assertTrue(self.permission.has_permission(self.request, None))
 
     def test_permission_doesnt_pass_authenticated_user(self):
         user = User.objects.create_user(email='test@test.com', password='qwe123!@#')
-        request = self.get_mock_request(user)
-        self.assertFalse(self.permission.has_permission(request, None))
+        self.request.user = user
+        self.assertFalse(self.permission.has_permission(self.request, None))
