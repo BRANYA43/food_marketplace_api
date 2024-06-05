c7269479fdf95a5ebbf33f30e8124d27f3bb58ac
BRANYA <tbd2504@gmail.com>
2024-06-05 15:55:38 +0300

    Update api docs

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,6 +1,9 @@
 from django.conf import settings
 from django.utils.module_loading import import_string
-from drf_spectacular.utils import extend_schema, OpenApiResponse
+from django.utils.translation import gettext as _
+
+from drf_spectacular.utils import extend_schema, OpenApiResponse, extend_schema_view
+from drf_standardized_errors import openapi_serializers
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
@@ -12,6 +15,75 @@ from accounts import serializers
 from accounts.permissions import IsUnauthenticated
 
 
+@extend_schema(tags=['Accounts'])
+@extend_schema_view(
+    register=extend_schema(
+        operation_id='user_register',
+        summary=_('Register user.'),
+        description=_('Register user by email, password, phone and full_name.'),
+        responses={
+            status.HTTP_201_CREATED: OpenApiResponse(
+                description=_('User registered successfully.'),
+            ),
+            status.HTTP_400_BAD_REQUEST: OpenApiResponse(
+                description=_('Invalid credentials.'),
+                response=openapi_serializers.ValidationErrorResponseSerializer,
+            ),
+        },
+    ),
+    login=extend_schema(
+        operation_id='user_login',
+        summary=_('Log a user in.'),
+        description=_('Log a user in by creating new refresh and access user tokens.'),
+        responses={
+            status.HTTP_200_OK: OpenApiResponse(
+                description='User logged in successfully.',
+                response=import_string(settings.SIMPLE_JWT['TOKEN_OBTAIN_SERIALIZER']),
+            ),
+            status.HTTP_400_BAD_REQUEST: OpenApiResponse(
+                description=_('Invalid credentials.'),
+                response=openapi_serializers.ValidationErrorResponseSerializer,
+            ),
+            status.HTTP_401_UNAUTHORIZED: OpenApiResponse(
+                description=_("Account isn't activate."),
+                response=openapi_serializers.ErrorResponse401Serializer,
+            ),
+            status.HTTP_403_FORBIDDEN: OpenApiResponse(
+                description=_('User already authenticated.'),
+                response=openapi_serializers.ErrorResponse403Serializer,
+            ),
+        },
+    ),
+    logout=extend_schema(
+        operation_id='user_logout',
+        summary=_('Log a user out.'),
+        description=_('Log a user out by adding all user tokens to the blacklist.'),
+        responses={
+            status.HTTP_200_OK: OpenApiResponse(
+                description=_('User logged out successfully.'),
+            ),
+            status.HTTP_401_UNAUTHORIZED: OpenApiResponse(
+                description=_('Credentials were not provided; Token is invalid, expired or blacklisted;'),
+                response=openapi_serializers.ErrorResponse401Serializer,
+            ),
+        },
+    ),
+    refresh=extend_schema(
+        operation_id='user_refresh_token',
+        summary=_('Refresh token.'),
+        description=_('Refresh a expiry of user refresh token and return new user access token.'),
+        responses={
+            status.HTTP_200_OK: OpenApiResponse(
+                description=_('Token was refreshed successfully.'),
+                response=import_string(settings.SIMPLE_JWT['TOKEN_REFRESH_SERIALIZER']),
+            ),
+            status.HTTP_401_UNAUTHORIZED: OpenApiResponse(
+                description=_('Token is invalid, expired or blacklisted;'),
+                response=openapi_serializers.ErrorResponse401Serializer,
+            ),
+        },
+    ),
+)
 class UserViewSet(viewsets.ViewSet):
     serializer_classes = {
         'register': serializers.UserRegisterSerializer,
@@ -30,15 +102,6 @@ class UserViewSet(viewsets.ViewSet):
         serializer = self.get_serializer_class()
         return serializer(*args, **kwargs)
 
-    @extend_schema(
-        operation_id='user_register',
-        description='Register user by email and password.',
-        responses={
-            201: OpenApiResponse(
-                description='User was registered successfully.',
-            ),
-        },
-    )
     @action(detail=False, methods=['post'], permission_classes=[IsUnauthenticated])
     def register(self, request):
         serializer = self.get_serializer(data=request.data)
@@ -46,37 +109,14 @@ class UserViewSet(viewsets.ViewSet):
         serializer.save()
         return Response(status=status.HTTP_201_CREATED)
 
-    @extend_schema(
-        operation_id='user_login',
-        description='Log a user in by creating new refresh and access user tokens.',
-        responses={
-            200: OpenApiResponse(description='User logged in successfully.', response=serializer_classes['login']),
-        },
-    )
     @action(detail=False, methods=['post'], permission_classes=[IsUnauthenticated])
     def login(self, request):
         return jwt_views.token_obtain_pair(request._request)
 
-    @extend_schema(
-        operation_id='user_logout',
-        description='Log a user out by adding all user tokens to the blacklist.',
-        responses={
-            200: OpenApiResponse(description='User logged out successfully.'),
-        },
-    )
     @action(detail=False, methods=['post'], permission_classes=[IsAuthenticated])
     def logout(self, request):
         return jwt_views.token_blacklist(request._request)
 
-    @extend_schema(
-        operation_id='user_refresh_token',
-        description='Refresh a expiry of user refresh token and return new user access token.',
-        responses={
-            200: OpenApiResponse(
-                description='Token was refreshed successfully.', response=serializer_classes['refresh']
-            ),
-        },
-    )
     @action(detail=False, methods=['post'], permission_classes=[AllowAny])
     def refresh(self, request):
         return jwt_views.token_refresh(request._request)
