cd4da9e65bc15892718ea9b9d1275a921c83822b
BRANYA <tbd2504@gmail.com>
2024-06-06 16:31:19 +0300

    Refactor tests of managers

--- src/accounts/tests/test_managers.py
+++ src/accounts/tests/test_managers.py
@@ -1,17 +1,18 @@
 from accounts import managers
 from accounts import models
-from django.test import TestCase
+
+from utils.tests import APITestCase
 
 
-class UserManagerTest(TestCase):
+class UserManagerTest(APITestCase):
     def setUp(self) -> None:
         self.manager = managers.UserManager()
         self.manager.model = models.User
 
-        self.data = {
-            'email': 'test@test.com',
-            'password': 'qwe123!@#',
-        }
+        self.data = dict(
+            email=self.TEST_EMAIL,
+            password=self.TEST_PASSWORD,
+        )
 
     def test_manager_doesnt_create_user_or_superuser_if_email_is_none(self):
         self.data['email'] = None
@@ -31,7 +32,7 @@ class UserManagerTest(TestCase):
         with self.assertRaisesRegex(ValueError, r'Password cannot be empty.'):
             self.manager.create_superuser(**self.data)
 
-    def test_manager_creates_user_correct(self):
+    def test_manager_creates_user_correctly(self):
         user = self.manager.create_user(**self.data)
         self.assertEqual(user.email, self.data['email'])
         self.assertTrue(user.check_password(self.data['password']))
@@ -41,7 +42,7 @@ class UserManagerTest(TestCase):
         self.assertEqual(user.is_staff, False)
         self.assertEqual(user.is_superuser, False)
 
-    def test_manager_create_superuser_correct(self):
+    def test_manager_create_superuser_correctly(self):
         superuser = self.manager.create_superuser(**self.data)
         self.assertEqual(superuser.email, self.data['email'])
         self.assertTrue(superuser.check_password(self.data['password']))
