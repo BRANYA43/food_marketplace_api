5f40dba18212410884e249af6b6c9bc901f360aa
BRANYA <tbd2504@gmail.com>
2024-06-25 17:56:02 +0300

    Create advert model

--- src/catalogs/models.py
+++ src/catalogs/models.py
@@ -1,5 +1,11 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.db.models import Q
 from django.utils.translation import gettext as _
+
+
+def _get_default_grades() -> dict:
+    return {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0}
 
 
 class CreatedUpdatedMixin(models.Model):
@@ -43,3 +49,62 @@ class Category(CreatedUpdatedMixin):
 
     def __str__(self):
         return self.name
+
+
+class Advert(CreatedUpdatedMixin):
+    user = models.ForeignKey(
+        verbose_name=_('user'),
+        to=get_user_model(),
+        on_delete=models.PROTECT,
+    )
+    category = models.ForeignKey(
+        verbose_name=_('category'),
+        to=Category,
+        on_delete=models.PROTECT,
+    )
+    title = models.CharField(
+        verbose_name=_('title'),
+        max_length=100,
+        unique_for_date='created_at',
+    )
+    price = models.DecimalField(
+        verbose_name=_('price'),
+        max_digits=12,
+        decimal_places=2,
+    )
+    descr = models.TextField(
+        verbose_name=_('description'),
+        blank=True,
+        null=True,
+    )
+    address = models.CharField(
+        verbose_name=_('address'),
+        max_length=256,
+    )
+    grades = models.JSONField(
+        verbose_name=_('grades'),
+        default=_get_default_grades,
+    )
+    is_displayed = models.BooleanField(
+        verbose_name=_('displayed'),
+        default=True,
+    )
+
+    class Meta:
+        verbose_name = _('advert')
+        verbose_name_plural = _('adverts')
+        ordering = ('is_displayed', 'created_at')
+        constraints = (
+            models.CheckConstraint(
+                name='price_gte_0',
+                check=Q(price__gte=0),
+            ),
+        )
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def grade(self) -> int | float:
+        totals = [int(grade) * count for grade, count in self.grades.items()]
+        return round(sum(totals) / 5)
--- src/catalogs/tests/test_models.py
+++ src/catalogs/tests/test_models.py
@@ -1,8 +1,97 @@
+from django.contrib.auth import get_user_model
+from django.utils.timezone import timedelta, now
 from django.core.exceptions import ValidationError
 from django.db import IntegrityError
 from django.test import TestCase
 
 from catalogs import models
+
+
+class AdvertModelTest(TestCase):
+    def setUp(self) -> None:
+        self.model_class = models.Advert
+        self.user = get_user_model().objects.create_user(email='test@test.com', password='qwe123!@#')
+        self.category = models.Category.objects.create(name='vegetables')
+        self.data = dict(
+            user=self.user,
+            category=self.category,
+            title='Potato',
+            price='100.00',
+            address='potato street',
+        )
+
+    def test_user_is_required(self):
+        del self.data['user']
+        with self.assertRaisesRegex(IntegrityError, r'NOT NULL'):
+            self.model_class.objects.create(**self.data)
+
+    def test_category_is_required(self):
+        del self.data['category']
+        with self.assertRaisesRegex(IntegrityError, r'NOT NULL'):
+            self.model_class.objects.create(**self.data)
+
+    def test_title_field_is_required(self):
+        del self.data['title']
+        advert = self.model_class.objects.create(**self.data)
+        with self.assertRaisesRegex(ValidationError, r'This field cannot be blank.'):
+            advert.full_clean()
+
+    def test_title_field_is_unique_for_date(self):
+        advert = self.model_class.objects.create(**self.data)
+        with self.assertRaisesRegex(ValidationError, r'Title must be unique for Created date.'):
+            advert_ = self.model_class.objects.create(**self.data)
+            advert_.full_clean()
+
+        advert.created_at = now() - timedelta(days=1)
+        advert.save()
+
+        self.model_class.objects.create(**self.data)  # not raise
+
+    def test_price_field_is_required(self):
+        del self.data['price']
+        with self.assertRaisesRegex(IntegrityError, r'NOT NULL'):
+            self.model_class.objects.create(**self.data)
+
+    def test_price_field_cannot_be_less_0(self):
+        self.data['price'] = -1
+        with self.assertRaisesRegex(IntegrityError, r'CHECK'):
+            self.model_class.objects.create(**self.data)
+
+    def test_price_field_can_be_equal_0(self):
+        self.data['price'] = 0
+        advert = self.model_class.objects.create(**self.data)
+        advert.full_clean()  # not raise
+
+    def test_address_field_is_required(self):
+        del self.data['address']
+        advert = self.model_class.objects.create(**self.data)
+        with self.assertRaisesRegex(ValidationError, r'This field cannot be blank'):
+            advert.full_clean()
+
+    def test_descr_field_is_optional(self):
+        advert = self.model_class.objects.create(**self.data)
+        advert.full_clean()  # not raise
+        self.assertIsNone(advert.descr)
+
+    def test_grades_field_is_expected_dict_by_default(self):
+        expected_dict = {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0}
+        advert = self.model_class.objects.create(**self.data)
+
+        self.assertDictEqual(advert.grades, expected_dict)
+
+    def test_is_disabled_field_is_true_by_default(self):
+        advert = self.model_class.objects.create(**self.data)
+        self.assertTrue(advert.is_displayed)
+
+    def test_string_representation_is_title(self):
+        advert = self.model_class.objects.create(**self.data)
+        self.assertEqual(str(advert), advert.title)
+
+    def test_grade_property_returns_correct_grade(self):
+        grades = {'1': 1, '2': 1, '3': 1, '4': 1, '5': 1}
+        advert = self.model_class.objects.create(**self.data, grades=grades)
+
+        self.assertEqual(advert.grade, 3)
 
 
 class CategoryModelTest(TestCase):
