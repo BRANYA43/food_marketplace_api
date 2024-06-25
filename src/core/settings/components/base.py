d898be047582098f05d5b1794bf558331b82a54f
BRANYA <tbd2504@gmail.com>
2024-06-25 14:33:16 +0300

    Create catalogs app

--- src/core/settings/components/base.py
+++ src/core/settings/components/base.py
@@ -20,6 +20,7 @@ INSTALLED_APPS = [
     'django_filters',
     'mail_templated',
     'accounts',
+    'catalogs',
     'utils',
 ]
 
--- src/core/urls.py
+++ src/core/urls.py
@@ -26,6 +26,7 @@ api_urls = [
     path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path('account/', include('accounts.urls')),
+    path('catalog/', include('catalogs.urls')),
 ]
 
 urlpatterns = [
