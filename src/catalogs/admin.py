c118fce289f237155a2b54a4c536b3601776bb9e
BRANYA <tbd2504@gmail.com>
2024-07-05 14:45:49 +0300

    Add category to admin site

--- src/catalogs/admin.py
+++ src/catalogs/admin.py
@@ -1,1 +1,23 @@
-# Register your models here.
+from django.contrib import admin
+
+from catalogs import models
+
+
+class CategoryInline(admin.TabularInline):
+    model = models.Category
+    fields = ('name',)
+    extra = 1
+    show_change_link = True
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at')
+    fieldsets = (
+        ('Info', dict(fields=('name',))),
+        ('Dates', dict(fields=('updated_at', 'created_at'))),
+    )
+    readonly_fields = ('updated_at', 'created_at')
+    inlines = (CategoryInline,)
+    search_fields = ('name',)
+    ordering = ('name',)
