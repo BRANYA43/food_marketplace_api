0f9f7a054ddd4acd397a7422552a4441f055f1b5
BRANYA <tbd2504@gmail.com>
2024-07-10 13:25:09 +0300

    Create advert address serializer

--- src/catalogs/serializers.py
+++ src/catalogs/serializers.py
@@ -3,6 +3,26 @@ from rest_framework import serializers
 from catalogs import models
 
 
+class AdvertAddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.AdvertAddress
+        fields = ('advert', 'region', 'city', 'village', 'street', 'number')
+        extra_kwargs = dict(advert=dict(required=False, write_only=True))
+
+    def update(self, instance, validated_data):
+        for field, value in validated_data.items():
+            setattr(instance, field, value)
+        instance.full_clean()
+        instance.save()
+        return instance
+
+    def create(self, validated_data):
+        instance = models.AdvertAddress(**validated_data)
+        instance.full_clean()
+        instance.save()
+        return instance
+
+
 class _RecurseSerializer(serializers.Serializer):
     def to_representation(self, instance):
         serializer = self.parent.parent.__class__(instance, context=self.context)
--- src/catalogs/tests/test_serializers.py
+++ src/catalogs/tests/test_serializers.py
@@ -1,7 +1,114 @@
 from django.test import TestCase
+from rest_framework.exceptions import ValidationError
 from rest_framework.serializers import ModelSerializer
 
 from catalogs import serializers, models
+from utils.tests import APITestCase
+
+
+class AdvertAddressSerializerTest(APITestCase):
+    def setUp(self) -> None:
+        self.serializer_class = serializers.AdvertAddressSerializer
+        self.address_model_class = models.AdvertAddress
+        self.user = self.create_test_user()
+        self.category = models.Category.objects.create(name='vegetables')
+        self.advert = models.Advert.objects.create(
+            user=self.user,
+            category=self.category,
+            title='Potato',
+            price='100.00',
+        )
+        self.data = dict(
+            region='region',
+            city='city',
+            street='street',
+            number='0',
+        )
+
+    def test_serializer_inherit_model_serializer(self):
+        self.assertTrue(issubclass(self.serializer_class, ModelSerializer))
+
+    def test_advert_field_is_optional(self):
+        serializer = self.serializer_class(data=self.data)
+        self.assertTrue(serializer.is_valid(raise_exception=True))
+
+    def test_advert_field_is_write_only(self):
+        self.data['advert'] = self.advert.pk
+        serializer = self.serializer_class(data=self.data)
+        self.assertTrue(serializer.is_valid())
+
+        self.assertIsNone(serializer.data.get('user'))
+
+    def test_region_field_is_required(self):
+        del self.data['region']
+        serializer = self.serializer_class(data=self.data)
+        with self.assertRaisesRegex(ValidationError, r"code='required'"):
+            serializer.is_valid(raise_exception=True)
+
+    def test_city_field_is_required(self):
+        del self.data['city']
+        serializer = self.serializer_class(data=self.data)
+        with self.assertRaisesRegex(ValidationError, r"code='required'"):
+            serializer.is_valid(raise_exception=True)
+
+    def test_street_field_is_required(self):
+        del self.data['street']
+        serializer = self.serializer_class(data=self.data)
+        with self.assertRaisesRegex(ValidationError, r"code='required'"):
+            serializer.is_valid(raise_exception=True)
+
+    def test_number_field_is_required(self):
+        del self.data['number']
+        serializer = self.serializer_class(data=self.data)
+        with self.assertRaisesRegex(ValidationError, r"code='required'"):
+            serializer.is_valid(raise_exception=True)
+
+    def test_village_field_is_optional(self):
+        serializer = self.serializer_class(data=self.data)
+        self.assertTrue(serializer.is_valid())
+
+    def test_serializer_creates_address(self):
+        self.assertEqual(self.address_model_class.objects.count(), 0)
+
+        self.data['advert'] = self.advert.pk
+        serializer = self.serializer_class(data=self.data)
+        self.assertTrue(serializer.is_valid())
+        serializer.save()
+
+        address = self.address_model_class.objects.get(advert=self.advert)
+        del self.data['advert']
+        for field, value in self.data.items():
+            self.assertEqual(getattr(address, field, value), self.data[field])
+
+    def test_serializer_updates_address(self):
+        self.data['advert'] = self.advert
+        address = self.address_model_class.objects.create(**self.data)
+
+        update_data = dict(region='new region')
+        serializer = self.serializer_class(address, update_data, partial=True)
+        self.assertTrue(serializer.is_valid(raise_exception=True))
+        serializer.save()
+
+        db_address = self.address_model_class.objects.get(advert=self.advert)
+
+        self.assertEqual(db_address.region, update_data['region'])
+
+    def test_serializer_returns_expected_data(self):
+        self.data['advert'] = self.advert.pk
+
+        expected_data = dict(
+            region=self.data['region'],
+            city=self.data['city'],
+            village=None,
+            street=self.data['street'],
+            number=self.data['number'],
+        )
+
+        serializer = self.serializer_class(data=self.data)
+        self.assertTrue(serializer.is_valid(raise_exception=True))
+        serializer.save()
+
+        self.assertDictEqual(serializer.data, expected_data)
 
 
 class CategoryRetrieveSerializerTest(TestCase):
