eb0b7d39dcd8b1bcbcfa7473865a4278e74d493a
BRANYA <tbd2504@gmail.com>
2024-07-11 14:56:27 +0300

    Create advert serializer

--- src/catalogs/serializers.py
+++ src/catalogs/serializers.py
@@ -23,6 +23,61 @@ class AdvertAddressSerializer(serializers.ModelSerializer):
         return instance
 
 
+class AdvertSerializer(serializers.ModelSerializer):
+    address = AdvertAddressSerializer(required=False)
+
+    class Meta:
+        model = models.Advert
+        fields = (
+            'id',
+            'user',
+            'category',
+            'title',
+            'descr',
+            'price',
+            'address',
+            'use_pickup',
+            'use_nova_post',
+            'use_courier',
+        )
+        read_only_field = ('id',)
+
+    def create(self, validated_data):
+        address_data = validated_data.pop('address', None)
+        advert = models.Advert(**validated_data)
+        advert.full_clean()
+        advert.save()
+
+        if address_data is not None:
+            self._update_or_create_address(advert, address_data)
+
+        advert.refresh_from_db()
+        return advert
+
+    def update(self, instance, validated_data):
+        advert = instance
+        address_data = validated_data.pop('address', None)
+        for field, value in validated_data.items():
+            setattr(advert, field, value)
+        advert.full_clean()
+        advert.save()
+
+        if address_data is not None:
+            self._update_or_create_address(advert, address_data)
+
+        advert.refresh_from_db()
+        return advert
+
+    def _update_or_create_address(self, advert, address_data):
+        if (address := getattr(advert, 'address', None)) is not None:
+            serializer = AdvertAddressSerializer(address, address_data, partial=True)
+        else:
+            address_data['advert'] = advert.pk
+            serializer = AdvertAddressSerializer(data=address_data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+
 class _RecurseSerializer(serializers.Serializer):
     def to_representation(self, instance):
         serializer = self.parent.parent.__class__(instance, context=self.context)
--- src/catalogs/tests/test_serializers.py
+++ src/catalogs/tests/test_serializers.py
@@ -1,9 +1,216 @@
+from decimal import Decimal
+
 from django.test import TestCase
 from rest_framework.exceptions import ValidationError
 from rest_framework.serializers import ModelSerializer
 
 from catalogs import serializers, models
 from utils.tests import APITestCase
+
+
+class AdvertSerializerTest(APITestCase):
+    def setUp(self) -> None:
+        self.serializer_class = serializers.AdvertSerializer
+        self.user = self.create_test_user()
+        self.category = models.Category.objects.create(name='vegetables')
+        self.address_data = dict(
+            region='region',
+            city='city',
+            street='street',
+            number='number',
+        )
+        self.advert_data = dict(
+            user=self.user,
+            category=self.category,
+            title='Potato',
+            price='100.00',
+        )
+        self.data = dict(
+            user=self.user.id,
+            category=self.category.id,
+            title='Potato',
+            price='100.00',
+            address=self.address_data,
+        )
+
+    def test_serializer_inherits_model_serializer(self):
+        self.assertTrue(issubclass(self.serializer_class, ModelSerializer))
+
+    def test_address_field_is_optional(self):
+        del self.data['address']
+        serializer = self.serializer_class(data=self.data)
+        self.assertTrue(serializer.is_valid(raise_exception=True))  # not raise
+        self.assertIsNone(serializer.data.get('address'))
+
+    def test_serializer_creates_advert_without_address(self):
+        del self.data['address']
+        self.assertEqual(models.Advert.objects.count(), 0)
+        self.assertEqual(models.AdvertAddress.objects.count(), 0)
+
+        serializer = self.serializer_class(data=self.data)
+        self.assertTrue(serializer.is_valid(raise_exception=True))
+        serializer.save()
+
+        self.assertEqual(models.Advert.objects.count(), 1)
+        self.assertEqual(models.AdvertAddress.objects.count(), 0)
+
+        advert = models.Advert.objects.first()
+
+        self.assertEqual(advert.user.id, self.data['user'])
+        self.assertEqual(advert.category.id, self.data['category'])
+        self.assertEqual(advert.title, self.data['title'])
+        self.assertIsNone(advert.descr)
+        self.assertEqual(advert.price, Decimal(self.data['price']))
+        self.assertFalse(advert.use_pickup)
+        self.assertFalse(advert.use_nova_post)
+        self.assertTrue(advert.use_courier)
+
+    def test_serializer_creates_advert_together_with_address(self):
+        self.assertEqual(models.Advert.objects.count(), 0)
+        self.assertEqual(models.AdvertAddress.objects.count(), 0)
+
+        serializer = self.serializer_class(data=self.data)
+        self.assertTrue(serializer.is_valid(raise_exception=True))
+        serializer.save()
+
+        self.assertEqual(models.Advert.objects.count(), 1)
+        self.assertEqual(models.AdvertAddress.objects.count(), 1)
+
+        advert = models.Advert.objects.first()
+
+        self.assertEqual(advert.user.id, self.data['user'])
+        self.assertEqual(advert.category.id, self.data['category'])
+        self.assertEqual(advert.title, self.data['title'])
+        self.assertIsNone(advert.descr)
+        self.assertEqual(advert.price, Decimal(self.data['price']))
+        self.assertFalse(advert.use_pickup)
+        self.assertFalse(advert.use_nova_post)
+        self.assertTrue(advert.use_courier)
+
+        address = models.AdvertAddress.objects.first()
+
+        self.assertEqual(address.advert.id, address.id)
+        self.assertEqual(address.region, self.address_data['region'])
+        self.assertEqual(address.city, self.address_data['city'])
+        self.assertIsNone(address.village)
+        self.assertEqual(address.street, self.address_data['street'])
+        self.assertEqual(address.number, self.address_data['number'])
+
+    def test_serializer_updates_advert_without_address(self):
+        update_data = dict(title='new title')
+        advert = models.Advert.objects.create(**self.advert_data)
+
+        self.assertEqual(models.Advert.objects.count(), 1)
+        self.assertEqual(models.AdvertAddress.objects.count(), 0)
+        self.assertNotEqual(advert.title, update_data['title'])
+
+        serializer = self.serializer_class(advert, update_data, partial=True)
+        self.assertTrue(serializer.is_valid(raise_exception=True))
+        serializer.save()
+
+        self.assertEqual(models.Advert.objects.count(), 1)
+        self.assertEqual(models.AdvertAddress.objects.count(), 0)
+        self.assertEqual(advert.title, update_data['title'])
+
+    def test_serializer_updates_advert_and_create_address(self):
+        update_data = dict(
+            title='new title',
+            address=self.address_data,
+        )
+        advert = models.Advert.objects.create(**self.advert_data)
+
+        self.assertEqual(models.Advert.objects.count(), 1)
+        self.assertEqual(models.AdvertAddress.objects.count(), 0)
+        self.assertNotEqual(advert.title, update_data['title'])
+
+        serializer = self.serializer_class(advert, update_data, partial=True)
+        self.assertTrue(serializer.is_valid(raise_exception=True))
+        serializer.save()
+
+        self.assertEqual(models.Advert.objects.count(), 1)
+        self.assertEqual(models.AdvertAddress.objects.count(), 1)
+        self.assertEqual(advert.title, update_data['title'])
+
+        address = models.AdvertAddress.objects.first()
+
+        self.assertEqual(address.region, self.address_data['region'])
+        self.assertEqual(address.city, self.address_data['city'])
+        self.assertIsNone(address.village)
+        self.assertEqual(address.street, self.address_data['street'])
+        self.assertEqual(address.number, self.address_data['number'])
+
+    def test_serializer_updates_advert_together_with_address(self):
+        update_data = dict(
+            title='new title',
+            address=dict(number='new number'),
+        )
+        advert = models.Advert.objects.create(**self.advert_data)
+        address = models.AdvertAddress.objects.create(**self.address_data, advert=advert)
+        self.assertEqual(models.Advert.objects.count(), 1)
+        self.assertEqual(models.AdvertAddress.objects.count(), 1)
+        self.assertNotEqual(advert.title, update_data['title'])
+        self.assertNotEqual(address.number, update_data['address']['number'])
+
+        serializer = self.serializer_class(advert, update_data, partial=True)
+        self.assertTrue(serializer.is_valid(raise_exception=True))
+        serializer.save()
+
+        address.refresh_from_db()
+
+        self.assertEqual(models.Advert.objects.count(), 1)
+        self.assertEqual(models.AdvertAddress.objects.count(), 1)
+        self.assertEqual(advert.title, update_data['title'])
+        self.assertEqual(address.number, update_data['address']['number'])
+
+    def test_serializer_returns_expected_data_without_address(self):
+        del self.data['address']
+        serializer = self.serializer_class(data=self.data)
+        self.assertTrue(serializer.is_valid(raise_exception=True))
+        serializer.save()
+
+        advert = models.Advert.objects.first()
+        expected_data = dict(
+            id=advert.id,
+            user=advert.user.id,
+            category=advert.category.id,
+            title=advert.title,
+            descr=advert.descr,
+            price=str(advert.price),
+            address=getattr(advert, 'address', None),
+            use_pickup=advert.use_pickup,
+            use_nova_post=advert.use_nova_post,
+            use_courier=advert.use_courier,
+        )
+
+        self.assertDictEqual(serializer.data, expected_data)
+
+    def test_serializer_returns_expected_data_with_address(self):
+        serializer = self.serializer_class(data=self.data)
+        self.assertTrue(serializer.is_valid(raise_exception=True))
+        serializer.save()
+
+        advert = models.Advert.objects.first()
+        address = advert.address
+        expected_data = dict(
+            id=advert.id,
+            user=advert.user.id,
+            category=advert.category.id,
+            title=advert.title,
+            descr=advert.descr,
+            price=str(advert.price),
+            address=dict(
+                region=address.region,
+                city=address.city,
+                village=address.village,
+                street=address.street,
+                number=address.number,
+            ),
+            use_pickup=advert.use_pickup,
+            use_nova_post=advert.use_nova_post,
+            use_courier=advert.use_courier,
+        )
+
+        self.assertDictEqual(serializer.data, expected_data)
 
 
 class AdvertAddressSerializerTest(APITestCase):
