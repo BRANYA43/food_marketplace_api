cbf8679bac49314441767bdd1aed47ab5d88b7ff
BRANYA <tbd2504@gmail.com>
2024-07-12 14:23:13 +0300

    Add asserts for model field to api test case

--- src/utils/tests/api_test_case.py
+++ src/utils/tests/api_test_case.py
@@ -1,5 +1,7 @@
 from typing import Literal, Sequence
 
+from django.core.exceptions import ValidationError
+from django.db import IntegrityError
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.test import APITestCase, APIRequestFactory
@@ -37,3 +39,45 @@ class ApiTestCase(APITestCase):
         for method in methods:
             response = getattr(self.client, method)(url)
             self.assert_response_status(response, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def assert_required_model_field(self, model, field: str, data: dict, detail: str):
+        del data[field]
+        regex = rf'.*{detail}.*'
+        with self.assertRaisesRegex(
+            (ValidationError, IntegrityError),
+            regex,
+            msg=f'This field "{field}" is not required.',
+        ):
+            instance = model(**data)
+            instance.full_clean()
+
+    def assert_optional_model_field(self, model, field: str, data: dict):
+        if field in data:
+            del data[field]
+        instance = model(**data)
+        try:
+            instance.full_clean()
+        except (ValidationError, IntegrityError):
+            raise AssertionError(f'This field "{field}" is not optional. It is required field.')
+        self.assertIsNone(
+            getattr(instance, field), msg=f'This field "{field}" is not optional. It has value by default.'
+        )
+
+    def assert_model_field_default_value(self, model, field: str, data: dict, expected_value):
+        instance = model(**data)
+        field_value = getattr(instance, field)
+        if isinstance(expected_value, bool):
+            if expected_value:
+                self.assertTrue(field_value, msg='Expected default value "True", but got "False".')
+            else:
+                self.assertFalse(field_value, msg='Expected default value "False", but got "True".')
+        else:
+            self.assertEqual(
+                field_value, expected_value, msg=f'Expected default value "{expected_value}", but got "{field_value}".'
+            )
+
+    def assert_validation_model_field(self, model, data: dict, detail: str):
+        regex = rf'.*{detail}.*'
+        with self.assertRaisesRegex(ValidationError, regex):
+            instance = model(**data)
+            instance.full_clean()
--- src/utils/tests/test_api_test_case.py
+++ src/utils/tests/test_api_test_case.py
@@ -1,6 +1,20 @@
 from unittest.mock import MagicMock, patch
 
+from django.core.validators import MinLengthValidator
+from django.db import models
+
 from utils.tests import ApiTestCase
+
+
+class TestModel(models.Model):
+    required_field = models.CharField(max_length=10, validators=[MinLengthValidator(2)])
+    optional_field = models.CharField(max_length=10, null=True, blank=True)
+    default_field = models.CharField(max_length=10, default='1')
+    default_true_field = models.BooleanField(default=True)
+    default_false_field = models.BooleanField(default=False)
+
+    class Meta:
+        managed = False
 
 
 class ApiTestCaseTest(ApiTestCase):
@@ -61,3 +75,59 @@ class ApiTestCaseTest(ApiTestCase):
             AssertionError, rf'Expected response status code "405", but got "{mock_response.status_code}".'
         ):
             self.assert_not_allowed_methods(url, ['get'])
+
+    def test_assert_required_model_field(self):
+        detail = 'This field cannot be blank'
+
+        self.assert_required_model_field(TestModel, 'required_field', dict(required_field='test'), detail)  # not raise
+
+        with self.assertRaisesRegex(AssertionError, r'This field "optional_field" is not required.'):
+            self.assert_required_model_field(
+                TestModel, 'optional_field', dict(required_field='test', optional_field='test'), detail
+            )
+
+    def test_assert_optional_model_field(self):
+        self.assert_optional_model_field(TestModel, 'optional_field', dict(required_field='test'))  # not raise
+
+        with self.assertRaisesRegex(
+            AssertionError,
+            r'This field "required_field" is not optional. It is required field.',
+        ):
+            self.assert_optional_model_field(TestModel, 'required_field', dict(required_field='test'))
+
+        with self.assertRaisesRegex(
+            AssertionError,
+            'This field "default_field" is not optional. It has value by default.',
+        ):
+            self.assert_optional_model_field(TestModel, 'default_field', dict(required_field='test'))
+
+    def test_assert_model_field_default_value(self):
+        self.assert_model_field_default_value(TestModel, 'default_field', dict(required_field='test'), '1')
+        self.assert_model_field_default_value(TestModel, 'default_true_field', dict(required_field='test'), True)
+        self.assert_model_field_default_value(TestModel, 'default_false_field', dict(required_field='test'), False)
+
+        with self.assertRaisesRegex(
+            AssertionError,
+            r'Expected default value "2", but got "1".',
+        ):
+            self.assert_model_field_default_value(TestModel, 'default_field', dict(required_field='test'), '2')
+
+        with self.assertRaisesRegex(AssertionError, r'Expected default value "True", but got "False".'):
+            self.assert_model_field_default_value(TestModel, 'default_false_field', dict(required_field='test'), True)
+
+        with self.assertRaisesRegex(AssertionError, r'Expected default value "False", but got "True".'):
+            self.assert_model_field_default_value(TestModel, 'default_true_field', dict(required_field='test'), False)
+
+    def test_assert_validation_model_field(self):
+        self.assert_validation_model_field(
+            TestModel,
+            dict(required_field='t'),
+            detail='Ensure this value has at least 2 characters',
+        )
+
+        with self.assertRaises(AssertionError):
+            self.assert_validation_model_field(
+                TestModel,
+                dict(required_field='test'),
+                detail='Ensure this value has at least 2 characters',
+            )
