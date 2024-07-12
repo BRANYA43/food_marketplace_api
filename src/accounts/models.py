e4861b9d4e6cf077eadf8fa9dc44693a1b754aed
BRANYA <tbd2504@gmail.com>
2024-07-12 14:28:57 +0300

    Create user model

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,1 +1,72 @@
-# Create your models here.
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.core.validators import MinLengthValidator
+from django.db import models
+from django.utils.translation import gettext as _
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(
+        verbose_name=_('email'),
+        unique=True,
+    )
+    full_name = models.CharField(
+        verbose_name=_('full name'),
+        max_length=100,
+        null=True,
+        blank=True,
+        validators=[MinLengthValidator(2)],
+    )
+    phone = models.CharField(
+        verbose_name=_('Phone Number'),
+        max_length=20,
+        null=True,
+        blank=True,
+    )
+    is_staff = models.BooleanField(
+        verbose_name=_('is staff'),
+        default=False,
+        help_text=_('Make the account as admin.'),
+    )
+    is_active = models.BooleanField(
+        verbose_name=_('is active'),
+        default=True,  # TODO Must be False by default. Only after confirmation of email it is True
+        help_text=_('Active|Disable the account.'),
+    )
+    updated_at = models.DateTimeField(
+        verbose_name=_('Last Updated Date'),
+        auto_now=True,
+    )
+    joined_at = models.DateTimeField(
+        verbose_name=_('Joined Date'),
+        auto_now_add=True,
+    )
+
+    USERNAME_FIELD = 'email'
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+        get_latest_by = 'joined_at'
+        constraints = [
+            models.CheckConstraint(
+                check=models.Q(
+                    phone__regex=r'^(\+38)\s(\(\d{3}\))\s(\d{3})\s(\d{4})$',
+                ),
+                name='phone_number_in_valid_format',
+                violation_error_code='invalid_phone_number_format',
+                violation_error_message=_('The phone number must be in the following format: +38 (012) 345 6789.'),
+            ),
+        ]
+
+    def __str__(self):
+        return str(self.email)
+
+    @property
+    def refresh_token(self):
+        return RefreshToken.for_user(self)
+
+    @property
+    def access_token(self):
+        return RefreshToken.for_user(self).access_token  # type: ignore
---
+++ src/accounts/tests/test_models.py
@@ -0,0 +1,110 @@
+from datetime import datetime
+
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from rest_framework_simplejwt.tokens import Token
+
+from accounts import models
+from utils.tests import ApiTestCase
+
+
+class UserModelTest(ApiTestCase):
+    model = models.User
+
+    def setUp(self) -> None:
+        self.data = dict(
+            email=self.TEST_EMAIL,
+            password=self.TEST_PASSWORD,
+        )
+
+    def test_model_inherits_permissions_mixin(self):
+        self.assert_is_subclass(self.model, PermissionsMixin)
+
+    def test_model_inherits_abstract_base_user(self):
+        self.assert_is_subclass(self.model, AbstractBaseUser)
+
+    def test_email_field_is_required(self):
+        self.assert_required_model_field(
+            self.model,
+            'email',
+            self.data,
+            'This field cannot be blank',
+        )
+
+    def test_email_field_is_username_field(self):
+        self.assertEqual(self.model.USERNAME_FIELD, 'email')
+
+    def test_full_name_field_is_optional(self):
+        self.assert_optional_model_field(self.model, 'full_name', self.data)
+
+    def test_full_name_field_has_2_min_length(self):
+        self.data['full_name'] = 'a'
+        self.assert_validation_model_field(self.model, self.data, 'Ensure this value has at least 2 characters')
+
+    def test_phone_field_is_optional(self):
+        self.assert_optional_model_field(self.model, 'phone', self.data)
+
+    def test_phone_field_must_be_at_correct_format(self):
+        # Check valid phones
+        user = self.model(**self.data, phone='+38 (012) 345 6789')
+        user.full_clean()  # not raise
+
+        # Check invalid phones
+        invalid_phones = [
+            '+380500000000',
+            '+38 050 000 00 00',
+            '+38 050 00 00 000',
+            '0500000000',
+            '050 000 00 00',
+            '050 00 00 000',
+            '+1 (050) 000 00-00',
+            '380500000000',
+            '050-000-0000',
+            '+38 050-00-00-00',
+            '050 00 00 00',
+            '+38 05000000 000',
+        ]
+        for phone in invalid_phones:
+            self.data['phone'] = phone
+            self.assert_validation_model_field(
+                self.model,
+                self.data,
+                r'The phone number must be in the following format: \+38 \(012\) 345 6789',
+            )
+
+    def test_is_staff_field_is_false_by_default(self):
+        self.assert_model_field_default_value(self.model, 'is_staff', self.data, False)
+
+    # TODO Must be False by default. Only after confirmation of email it is True
+    def test_is_active_field_is_true_by_default(self):
+        self.assert_model_field_default_value(self.model, 'is_active', self.data, True)
+
+    def test_updated_at_field_is_set_auto_after_every_save(self):
+        user = self.model.objects.create(**self.data)
+        self.assertAlmostEqual(user.updated_at.timestamp(), datetime.now().timestamp(), delta=1)
+
+        old_update_at = user.updated_at
+        user.full_name = 'Rick Sanchez'
+        user.save()
+        self.assertNotEqual(user.updated_at, old_update_at)
+
+    def test_joined_at_field_is_set_auto_only_at_first_save(self):
+        user = self.model.objects.create(**self.data)
+        self.assertAlmostEqual(user.joined_at.timestamp(), datetime.now().timestamp(), delta=1)
+
+        old_joined_at = user.joined_at
+        user.full_name = 'Rick Sanchez'
+        user.save()
+        self.assertEqual(user.joined_at, old_joined_at)
+
+    def test_access_token_property_returns_access_token(self):
+        user = self.model.objects.create(**self.data)
+        self.assertIsInstance(user.access_token, Token)
+
+    def test_refresh_token_returns_refresh_token(self):
+        user = self.model.objects.create(**self.data)
+        self.assertIsInstance(user.refresh_token, Token)
+
+    def test_refresh_token_and_access_token_dont_match(self):
+        user = self.model.objects.create(**self.data)
+        self.assertNotEqual(user.access_token, user.refresh_token)
