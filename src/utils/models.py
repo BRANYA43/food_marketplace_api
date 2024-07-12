1ed21e696c59d86e97915b1fae218fd7d09e28bb
BRANYA <tbd2504@gmail.com>
2024-07-12 15:58:57 +0300

    Create created updated mixin

--- src/utils/models.py
+++ src/utils/models.py
@@ -1,1 +1,16 @@
-# Create your models here.
+from django.db import models
+from django.utils.translation import gettext as _
+
+
+class CreatedUpdatedMixin(models.Model):
+    updated_at = models.DateTimeField(
+        verbose_name=_('last update'),
+        auto_now=True,
+    )
+    created_at = models.DateTimeField(
+        verbose_name=_('creation'),
+        auto_now_add=True,
+    )
+
+    class Meta:
+        abstract = True
