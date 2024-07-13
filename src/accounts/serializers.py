48653b7b249962ad8da921bafadd90101a921004
BRANYA <tbd2504@gmail.com>
2024-07-13 16:32:01 +0300

    Create phone number validation mixin

--- src/accounts/serializers.py
+++ src/accounts/serializers.py
@@ -1,9 +1,21 @@
 from django.contrib.auth.password_validation import validate_password
 
+from accounts.services import normalizers
+from accounts import validators
+
 
 class PasswordValidationMixin:
     """Serializer Mixin to validate a password"""
 
     def validate_password(self, value: str):
         validate_password(value, self.instance)  # type: ignore
+        return value
+
+
+class PhoneNumberValidationMixin:
+    """Serializer Mixin to validate a phone number"""
+
+    def validate_phone(self, value: str):
+        validators.UkrainianPhoneNumberValidator()(value)
+        value = normalizers.UkrainianPhoneNumberNormalizer()(value)
         return value
--- src/accounts/tests/test_serializer_mixins.py
+++ src/accounts/tests/test_serializer_mixins.py
@@ -1,10 +1,30 @@
 from django.contrib.auth import get_user_model
-from django.core.exceptions import ValidationError
+from rest_framework.exceptions import ValidationError as rest_ValidationError
+from django.core.exceptions import ValidationError as django_ValidationError
 
 from accounts import serializers
 from utils.tests import ApiTestCase
 
 User = get_user_model()
+
+
+class PhoneNumberMixinTest(ApiTestCase):
+    mixin = serializers.PhoneNumberValidationMixin
+
+    def test_validate_phone_method_doesnt_raise_and_return_valid_phone(self):
+        valid_phone = '+38(012)3456789'
+        self.mixin().validate_phone(valid_phone)  # not raises
+
+    def test_validate_phone_method_raises_error_for_invalid_digit_count(self):
+        invalid_phones = ['+38(012)345678', '(012)345678']
+        for phone in invalid_phones:
+            with self.assertRaisesRegex(rest_ValidationError, r'invalid_digit_count'):
+                self.mixin().validate_phone(phone)
+
+    def test_validate_phone_method_raises_error_for_invalid_country_code(self):
+        invalid_phone = '+10(012)345678'
+        with self.assertRaisesRegex(rest_ValidationError, r'invalid_country_code'):
+            self.mixin().validate_phone(invalid_phone)
 
 
 class PasswordValidationMixinTest(ApiTestCase):
@@ -20,7 +40,7 @@ class PasswordValidationMixinTest(ApiTestCase):
     def test_validate_password_method_raises_error_for_short_password(self):
         short_password = 'qwe123'
         with self.assertRaisesRegex(
-            ValidationError,
+            django_ValidationError,
             r'This password is too short. It must contain at least 8 characters.',
         ):
             self.mixin().validate_password(short_password)
@@ -28,7 +48,7 @@ class PasswordValidationMixinTest(ApiTestCase):
     def test_validate_password_method_raises_error_for_numeric_password(self):
         numeric_password = '12345678'
         with self.assertRaisesRegex(
-            ValidationError,
+            django_ValidationError,
             r'This password is entirely numeric.',
         ):
             self.mixin().validate_password(numeric_password)
@@ -36,7 +56,7 @@ class PasswordValidationMixinTest(ApiTestCase):
     def test_validate_password_method_raises_error_for_common_password(self):
         common_password = 'qwerty12'
         with self.assertRaisesRegex(
-            ValidationError,
+            django_ValidationError,
             r'This password is too common.',
         ):
             self.mixin().validate_password(common_password)
@@ -46,7 +66,7 @@ class PasswordValidationMixinTest(ApiTestCase):
         self.mixin.instance = user
         invalid_password = 'rick@test.com'
         with self.assertRaisesRegex(
-            ValidationError,
+            django_ValidationError,
             r'The password is too similar to the email.',
         ):
             self.mixin().validate_password(invalid_password)
