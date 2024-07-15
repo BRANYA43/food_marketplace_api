6708f2a69f2cb7812cba6f32396175119777b24e
BRANYA <tbd2504@gmail.com>
2024-07-15 15:31:22 +0300

    Remove unneed asserts for model

--- src/utils/tests/api_test_case.py
+++ src/utils/tests/api_test_case.py
@@ -43,42 +43,6 @@ class ApiTestCase(APITestCase):
             response = getattr(self.client, method)(url)
             self.assert_response_status(response, status.HTTP_405_METHOD_NOT_ALLOWED)
 
-    def assert_required_model_field(self, model, field: str, data: dict, detail: str):
-        del data[field]
-        regex = rf'.*{detail}.*'
-        with self.assertRaisesRegex(
-            (django_ValidationError, IntegrityError),
-            regex,
-            msg=f'This field "{field}" is not required.',
-        ):
-            instance = model(**data)
-            instance.full_clean()
-
-    def assert_optional_model_field(self, model, field: str, data: dict):
-        if field in data:
-            del data[field]
-        instance = model(**data)
-        try:
-            instance.full_clean()
-        except (django_ValidationError, IntegrityError):
-            raise AssertionError(f'This field "{field}" is not optional. It is required field.')
-        self.assertIsNone(
-            getattr(instance, field), msg=f'This field "{field}" is not optional. It has value by default.'
-        )
-
-    def assert_model_field_default_value(self, model, field: str, data: dict, expected_value):
-        instance = model(**data)
-        field_value = getattr(instance, field)
-        if isinstance(expected_value, bool):
-            if expected_value:
-                self.assertTrue(field_value, msg='Expected default value "True", but got "False".')
-            else:
-                self.assertFalse(field_value, msg='Expected default value "False", but got "True".')
-        else:
-            self.assertEqual(
-                field_value, expected_value, msg=f'Expected default value "{expected_value}", but got "{field_value}".'
-            )
-
     def assert_validation_model_field(self, model, data: dict, detail: str):
         regex = rf'.*{detail}.*'
         with self.assertRaisesRegex(django_ValidationError, regex):
