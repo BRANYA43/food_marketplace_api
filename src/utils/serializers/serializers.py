d797acaadd4aad5e3b2863c55cad98ba5bc7d0c5
BRANYA <tbd2504@gmail.com>
2024-07-16 20:00:48 +0300

    Add ability to work with generic manager

--- src/utils/serializers/serializers.py
+++ src/utils/serializers/serializers.py
@@ -1,3 +1,4 @@
+from django.db.models import Manager
 from rest_framework import serializers
 
 from utils import models
@@ -9,3 +10,13 @@ class AddressFieldSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Address
         fields = ('region', 'city', 'village', 'street', 'number')
+
+    def get_attribute(self, instance):
+        if isinstance(instance, Manager):
+            return instance.first()
+        return super().get_attribute(instance)
+
+    def to_representation(self, instance):
+        if isinstance(instance, Manager):
+            instance = instance.first()
+        return super().to_representation(instance)
