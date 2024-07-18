9fd81e4160e632bb7a2c7df3684dbe1d0b1e2a52
BRANYA <tbd2504@gmail.com>
2024-07-18 18:52:25 +0300

    Create category view set with category list view

--- src/catalogs/urls.py
+++ src/catalogs/urls.py
@@ -1,5 +1,9 @@
 from rest_framework.routers import DefaultRouter
 
+from catalogs import views
+
 router = DefaultRouter()
+router.register('category', views.CategoryViewSet)
+
 urlpatterns: list = []
 urlpatterns += router.urls
--- src/catalogs/views.py
+++ src/catalogs/views.py
@@ -1,1 +1,41 @@
-# Create your views here.
+from drf_spectacular.utils import extend_schema, extend_schema_view, OpenApiResponse
+from rest_framework import viewsets, status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+
+from catalogs.models import Category
+from catalogs.serializers import CategoryListSerializer
+
+
+@extend_schema(tags=['Catalog'])
+@extend_schema_view(
+    list=extend_schema(
+        operation_id='category-list',
+        summary='Get category list',
+        responses={
+            status.HTTP_200_OK: OpenApiResponse(
+                description='Category list is got successfully.',
+            ),
+            status.HTTP_204_NO_CONTENT: OpenApiResponse(
+                description='Category list is empty.',
+            ),
+        },
+    ),
+)
+class CategoryViewSet(viewsets.GenericViewSet):
+    model = Category
+    serializer_classes = dict(
+        list=CategoryListSerializer,
+    )
+    permission_classes = (AllowAny,)
+    queryset = Category.objects.filter(parent=None)
+
+    def get_serializer_class(self):
+        return self.serializer_classes[self.action]
+
+    def list(self, request):
+        serializer = self.get_serializer(self.get_queryset(), many=True)
+        if serializer.data:
+            return Response(serializer.data, status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_204_NO_CONTENT)
