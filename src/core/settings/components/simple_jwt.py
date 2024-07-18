ffc44f56740bf0cb0737d71e8d41eb5f1da3afc7
BRANYA <tbd2504@gmail.com>
2024-07-18 16:11:24 +0300

    Set BLACKLIST_AFTER_ROTATION as True

--- src/core/settings/components/simple_jwt.py
+++ src/core/settings/components/simple_jwt.py
@@ -11,7 +11,7 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
-    'BLACKLIST_AFTER_ROTATION': False,
+    'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': True,
     'ALGORITHM': 'HS256',
     'SIGNING_KEY': SECRET_KEY,
