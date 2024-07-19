f2e55378a5e774a0e4a42c851e6409a749822d96
BRANYA <tbd2504@gmail.com>
2024-07-19 19:40:04 +0300

    Create category model admin

---
+++ src/catalogs/admin/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from django.utils.translation import gettext as _
+
+from catalogs.models import Category
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_parent', 'is_child', 'updated_at', 'created_at')
+    fieldsets = (
+        ('Info', dict(fields=('name', 'is_parent', 'is_child'))),
+        ('Dates', dict(fields=('updated_at', 'created_at'))),
+    )
+    readonly_fields = ('is_parent', 'is_child', 'updated_at', 'created_at')
+    ordering = ('parent__name', 'name')
+
+    @admin.display(boolean=True, ordering=('is_parent',), description=_('Is Parent'))
+    def is_parent(self, instance: Category):
+        return instance.is_parent
+
+    @admin.display(boolean=True, ordering=('is_child',), description=_('Is Child'))
+    def is_child(self, instance: Category):
+        return instance.is_child
