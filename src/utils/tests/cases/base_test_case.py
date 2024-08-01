4117c2f5c46d2e04a50d4d869ba9d39a6b664a9c
BRANYA <tbd2504@gmail.com>
2024-08-01 19:58:51 +0300

    add creator and asserts methods to base test case

--- src/utils/tests/cases/base_test_case.py
+++ src/utils/tests/cases/base_test_case.py
@@ -1,5 +1,33 @@
 from rest_framework.test import APITestCase
 
+from accounts.models import User
+from catalogs.models import Category
+from utils.models import Address
+
 
 class BaseTestCase(APITestCase):
-    pass
+    ############
+    # Creators #
+    ############
+    TEST_EMAIL = 'rick.sanchez@test.com'
+    TEST_PASSWORD = 'rick123!@#'
+    TEST_PHONE = '+38 (012) 345 6789'
+    TEST_FULL_NAME = 'Rick Sanchez'
+
+    @staticmethod
+    def create_test_user(email=TEST_EMAIL, password=TEST_PASSWORD, **extra_fields) -> User:
+        return User.objects.create_user(email, password, **extra_fields)
+
+    @staticmethod
+    def create_test_address(content_obj, city='city', street='street', number='0', **extra_fields) -> Address:
+        return Address.objects.create(content_obj=content_obj, city=city, street=street, number=number, **extra_fields)
+
+    @staticmethod
+    def create_test_category(name='name', **extra_fields) -> Category:
+        return Category.objects.create(name=name, **extra_fields)
+
+    ###########
+    # Asserts #
+    ###########
+    def assert_is_subclass(self, __cls, __class_or_tuple):
+        self.assertTrue(issubclass(__cls, __class_or_tuple), msg=f'{__cls} is not subclass of {__class_or_tuple}.')
