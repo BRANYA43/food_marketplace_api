28db64017c9782053b51543dca725912fdfc5642
BRANYA <tbd2504@gmail.com>
2024-08-02 13:44:09 +0300

    create serializer test case

--- src/utils/tests/cases/__init__.py
+++ src/utils/tests/cases/__init__.py
@@ -1,4 +1,5 @@
 from .base_test_case import BaseTestCase
 from .model_test_case import ModelTestCase
+from .serializer_test_case import SerializerTestCase
 
-__all__ = ['BaseTestCase', 'ModelTestCase']
+__all__ = ['BaseTestCase', 'ModelTestCase', 'SerializerTestCase']
