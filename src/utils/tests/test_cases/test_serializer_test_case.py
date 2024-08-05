714ff1e408415cbffec79c1a81af98e394b9acb3
BRANYA <tbd2504@gmail.com>
2024-08-05 14:19:03 +0300

    move some test

--- src/utils/tests/test_cases/test_serializer_test_case.py
+++ src/utils/tests/test_cases/test_serializer_test_case.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
 
 from utils.tests.cases import SerializerTestCase
 
@@ -243,31 +242,6 @@ class SerializerTestCaseTest(SerializerTestCase):
             self.assert_fields_are_write_only(
                 TestSerializer,
                 ['not_writeonly'],
-            )
-
-    def test_create_serializer(self):
-        serializer = self.create_serializer(
-            TestSerializerForCreate,
-            dict(username='username', password='123'),
-            initial=dict(username='initial username'),
-        )
-
-        self.assertIsInstance(serializer, serializers.Serializer)
-        self.assertEqual(serializer.data, dict(username='username'))
-        # creator gives arguments to serializer
-        self.assertEqual(serializer.initial, dict(username='initial username'))
-
-        with self.assertRaisesRegex(ValidationError, r'password.+required'):
-            self.create_serializer(
-                TestSerializerForCreate,
-                dict(username='username'),
-            )
-
-        with self.assertRaisesRegex(NotImplementedError, r'.save\(\) method was called.'):
-            self.create_serializer(
-                TestSerializerForCreate,
-                dict(username='username', password='123'),
-                save=True,
             )
 
     def test_assert_output_serializer_data(self):
