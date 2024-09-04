9d5b0fd3af195e3c060061689ce11f8750823766
BRANYA43 <tbd2504@gmai.com>
2024-09-04 17:28:39 +0300

    add media test case to import

--- src/utils/tests/cases/__init__.py
+++ src/utils/tests/cases/__init__.py
@@ -2,5 +2,6 @@ from .base_test_case import BaseTestCase
 from .model_test_case import ModelTestCase
 from .serializer_test_case import SerializerTestCase
 from .view_test_case import ViewTestCase
+from .media_case import MediaTestCase
 
-__all__ = ['BaseTestCase', 'ModelTestCase', 'SerializerTestCase', 'ViewTestCase']
+__all__ = ['BaseTestCase', 'ModelTestCase', 'SerializerTestCase', 'ViewTestCase', 'MediaTestCase']
