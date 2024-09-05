6a05fb41df083be296100e70c8a34e434a6c1085
BRANYA43 <tbd2504@gmai.com>
2024-09-05 22:21:17 +0300

    add creator of test image

--- src/utils/tests/cases/media_case.py
+++ src/utils/tests/cases/media_case.py
@@ -1,9 +1,10 @@
 import io
 
-from PIL import Image
+from PIL import Image as PILImage
 from django.conf import settings
 from django.core.files.uploadedfile import SimpleUploadedFile
 
+from catalogs.models import Image, Advert
 from utils.tests.cases import BaseTestCase
 from django.test import override_settings
 from tempfile import TemporaryDirectory
@@ -20,8 +21,17 @@ class MediaTestCase(BaseTestCase):
 
     @staticmethod
     def get_image_simple_uploaded_file(image_name: str) -> SimpleUploadedFile:
-        image = Image.new('RGB', (100, 100), color=(255, 255, 255))
+        image = PILImage.new('RGB', (100, 100), color=(255, 255, 255))
         image_io = io.BytesIO()
         image.save(image_io, format='PNG')
         image_io.seek(0)
         return SimpleUploadedFile(image_name, image_io.read(), 'image/png')
+
+    @staticmethod
+    def create_test_image(
+        advert: Advert,
+        file: SimpleUploadedFile = get_image_simple_uploaded_file('image_name.png'),
+        type: Image.Type = Image.Type.MAIN,  # type: ignore
+        **extra_fields,
+    ) -> Image:
+        return Image.objects.create(advert=advert, file=file, type=type, **extra_fields)
