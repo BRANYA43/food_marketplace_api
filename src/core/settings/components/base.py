6af160f0f2037b3271fb94ab6dbe811b34511fcf
BRANYA43 <tbd2504@gmai.com>
2024-09-20 15:19:07 +0300

    Move drf_standardized_errors installing settings to base.py

--- src/core/settings/components/base.py
+++ src/core/settings/components/base.py
@@ -20,6 +20,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'drf_spectacular',
+    'drf_standardized_errors',
     'django_filters',
     'accounts',
     'catalogs',
--- src/core/settings/components/drf_standardized_errors.py
+++
@@ -1,5 +0,0 @@
-from core.settings.components.base import INSTALLED_APPS
-
-INSTALLED_APPS += [
-    'drf_standardized_errors',
-]
