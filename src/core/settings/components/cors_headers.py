e49821ade266c36de73e1a5b516e67cb12e20e08
Bogdan Timchenko | Богдан Тимченко <106832982+BRANYA43@users.noreply.github.com>
2024-09-21 19:28:58 +0300

    Add cors settings to cors_headers.py

--- src/core/settings/components/cors_headers.py
+++ src/core/settings/components/cors_headers.py
@@ -3,4 +3,9 @@ CORS Headers Settings
 GitHub: https://github.com/adamchainz/django-cors-headers
 """
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ORIGIN_WHITELIST = [
+    'https://web-app-marketplace-sviy.vercel.app',
+]
+CORS_ALLOW_METHODS = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+CORS_ALLOW_HEADERS = ['Content-Type', 'Authorization']
+CORS_ALLOW_CREDENTIALS = True
